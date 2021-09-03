b837e20f134f2e18a37c4230e6aca5145c5a8468
Your Name <you@example.com>
2021-09-03 12:06:24 -0500

    cambios

--- src/services/binarysearch.js
+++ src/services/binarysearch.js
@@ -1,6 +1,7 @@
 function binarysearch (arr, x, start, end) {
     if (start > end) return false;
     let mid=Math.floor((start + end)/2);
+    
     if (arr[mid].nombre === x) return arr[mid];     
     if(arr[mid].nombre > x)
         return binarysearch(arr, x, start, mid-1);
